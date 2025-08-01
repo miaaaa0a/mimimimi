279307645911a9aeadafeccefc05c757e019c97b
Mia <miaaaa0a@proton.me>
2025-08-01 10:27:46 +0300

    fix: optimize dithered bg rendering

--- src/components/Background.svelte
+++ src/components/Background.svelte
@@ -1,40 +1,32 @@
 <script>
     import { onMount } from 'svelte';
-    import { watchResize } from "svelte-watch-resize";
     var canvas;
     var innerWidth;
     var innerHeight;
 
     function draw_background() {
-        let width = canvas.width;
-        let height = canvas.height;
         let ctx = canvas.getContext("2d");
-        let image_data = ctx.createImageData(width, height);
+        
+        let patternCanvas = document.createElement('canvas');
+        patternCanvas.width = 2;
+        patternCanvas.height = 2;
+        let pCtx = patternCanvas.getContext('2d');
+        
+        pCtx.fillStyle = 'white';
+        pCtx.fillRect(0, 0, 2, 2);
+        pCtx.fillStyle = 'black';
+        pCtx.fillRect(0, 1, 1, 1);
+        
+        let pattern = ctx.createPattern(patternCanvas, 'repeat');
+        ctx.fillStyle = pattern;
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-        for (var x = 0; x < width; x++) {
-            for (var y = 0; y < height; y++) {
-                var index = (y * width + x) * 4;
-
-                if (x % 2 == 0 && y % 2 != 0) {
-                    image_data.data[index] = 0;
-                    image_data.data[index+1] = 0;
-                    image_data.data[index+2] = 0;
-                    image_data.data[index+3] = 255;
-                } else {
-                    image_data.data[index] = 255;
-                    image_data.data[index+1] = 255;
-                    image_data.data[index+2] = 255;
-                    image_data.data[index+3] = 255;
-                }
-            }
-        }
-
-        ctx.putImageData(image_data, 0, 0);
+        requestAnimationFrame(draw_background);
     }
 
     onMount(() => {
-        draw_background();
+        requestAnimationFrame(draw_background);
     });
 </script>
 <svelte:window bind:innerWidth={innerWidth} bind:innerHeight={innerHeight} />
-<canvas class="fixed w-screen h-screen z-[-9]" width="{innerWidth}" height="{innerHeight}" bind:this={canvas} use:watchResize={draw_background}></canvas>
+<canvas class="fixed w-screen h-screen z-[-9]" width="{innerWidth}" height="{innerHeight}" bind:this={canvas}></canvas>
